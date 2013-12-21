75f8171a87ebd9e580a60c00a3a72c0b1713240f
Janet Chuang <janet.chuang@gmail.com>
2013-12-21 18:32:40 -0500

    add author to books model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131221230612) do
+ActiveRecord::Schema.define(version: 20131221232946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20131221230612) do
     t.string   "title",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "author"
   end
 
 end

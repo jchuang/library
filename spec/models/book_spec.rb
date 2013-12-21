b21b6726768d6bc570680ecff415b0de37bf77b4
Janet Chuang <janet.chuang@gmail.com>
2013-12-21 18:44:54 -0500

    add optional rating to books

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,4 +4,7 @@ describe Book do
 
   it { should validate_presence_of :title }
 
+  it { should have_valid(:rating).when(0, 5, 27, 100, nil) }
+  it { should_not have_valid(:rating).when(-1, 33.3, 105, 'awesome')}
+
 end

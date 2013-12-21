8dbe75d5b28b42a5f5cd6dc3912a96501b4d7e55
Janet Chuang <janet.chuang@gmail.com>
2013-12-21 18:35:11 -0500

    add favorite (boolean) to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131221232946) do
+ActiveRecord::Schema.define(version: 20131221233300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: true do |t|
-    t.string   "title",      null: false
+    t.string   "title",                      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "author"
+    t.boolean  "favorite",   default: false
   end
 
 end

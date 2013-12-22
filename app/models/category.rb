68f47964f5ad23d4dfa98febb05db6afd043b4c8
Janet Chuang <janet.chuang@gmail.com>
2013-12-21 21:20:42 -0500

    seed categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  CATEGORY_NAMES = ['Fiction', 'Mystery', 'Cooking', 'Children', 'Young Adult']
+
+  def self.seed
+    CATEGORY_NAMES.each do |category_name|
+      unless Category.where(name: category_name).exists?
+        Category.create(name: category_name)
+      end
+    end
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,4 +7,17 @@ describe Category do
 
   it { should validate_uniqueness_of :name }
 
+  it 'should add categories to database' do
+    expect(Category.count).to eql(0)
+    Category.seed
+    expect(Category.count).to be > 0
+  end
+
+  it 'should not create duplicate categories' do
+    Category.seed
+    category_count = Category.count
+    Category.seed
+    expect(Category.count).to eql(category_count)
+  end
+
 end
